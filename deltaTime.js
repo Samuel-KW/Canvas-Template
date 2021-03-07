3d81bf6d40d2755c4a4ecee5586917a77c33eb2e
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-03-07 15:49:02 -0800

    Create deltaTime.js

---
+++ deltaTime.js
@@ -0,0 +1,32 @@
+class DeltaTime extends Game {
+    constructor() {
+        super();
+        
+        this.running = false;
+
+        this._time = 0;
+        this._loop = 0;
+    }
+
+    update (deltaTime) {
+        console.log(deltaTime);
+    }
+
+    loop (time) {
+        self.update(time - _time);
+
+        this._time = time;
+        this._loop = window.requestAnimationFrame(this.loop);
+    }
+
+
+    start_loop () {
+        this.running = true;
+        this._loop = window.requestAnimationFrame(this.loop);
+    }
+
+    stop_loop () {
+        this.running = false;
+        window.cancelAnimationFrame(this._loop);
+    }
+}
